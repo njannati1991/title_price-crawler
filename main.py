78e4379350300e16b9bf023c28824e9830aadece
Navid <njannati1991@gmail.com>
2024-07-15 16:29:24 +0330

    new featuer==> crawl both website

--- main.py
+++ main.py
@@ -26,3 +26,24 @@ if __name__ == "__main__":
 
         for title, price in zip(data[0], data[1]):
             print(title.text, price.text)
+
+    if target == 3:
+
+        crawler = LinksCrawler(ancher_text)
+        html_doc = crawler.get_page_source(TOROB)
+        data_torob = crawler.find_data(html_doc, TOROB_TITLE_CLASS, TOROB_PRICE_CLASS, TOROB_TITLE_TAG, TOROB_PRICE_TAG)
+
+        crawler = LinksCrawler(ancher_text)
+        html_doc = crawler.get_page_source(BASALAM)
+        data_basalam = crawler.find_data(
+            html_doc, BASALAM_TITLE_CLASS, BASALAM_PRICE_CLASS, BASALAM_TITLE_TAG, BASALAM_PRICE_TAG
+        )
+        print("Torob.com")
+        for title, price in zip(data_torob[0], data_torob[1]):
+            print(f"Title: {title.text}\t", f"Price: {price.text}")
+
+        print(40 * "*")
+
+        print("Basalam.com")
+        for title, price in zip(data_basalam[0], data_basalam[1]):
+            print(f"Title: {title.text}\t", f"Price: {price.text}")
