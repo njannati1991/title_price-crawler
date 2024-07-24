e7796a3ae0aae1f823e9fb39444da2aaf5e4a060
Navid <njannati1991@gmail.com>
2024-07-24 05:12:10 +0330

    modify main file

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from crawler import LinksCrawler
 from utils import *
 
-
 if __name__ == "__main__":
 
     ancher_text = 'laptop'
-    target = int(input('Please choice "Target" : 1.Torob, 2.Basalam: '))
+
+    target = int(input(f'''{200 * '*'}
+    1.Torob
+    2.Basalam
+    3.Torob and Basalam
+    Please choice "Target" : '''))
+    print(200 * '*')
 
     if target == 1:
         target_name = 'Torob.com'
@@ -30,14 +35,15 @@ if __name__ == "__main__":
     if target == 3:
 
         crawler = LinksCrawler(ancher_text)
+
         html_doc = crawler.get_page_source(TOROB)
         data_torob = crawler.find_data(html_doc, TOROB_TITLE_CLASS, TOROB_PRICE_CLASS, TOROB_TITLE_TAG, TOROB_PRICE_TAG)
 
-        crawler = LinksCrawler(ancher_text)
         html_doc = crawler.get_page_source(BASALAM)
         data_basalam = crawler.find_data(
             html_doc, BASALAM_TITLE_CLASS, BASALAM_PRICE_CLASS, BASALAM_TITLE_TAG, BASALAM_PRICE_TAG
         )
+
         print("Torob.com")
         for title, price in zip(data_torob[0], data_torob[1]):
             print(f"Title: {title.text}\t", f"Price: {price.text}")
